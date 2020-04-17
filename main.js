ad95b971efe927b405d24e6a79ced2ef361c7e25
Momo Ramadori <momoramadori8@gmail.com>
2020-04-17 13:14:21 +0200

    fatta la parte js

---
+++ main.js
@@ -0,0 +1,12 @@
+//inserire il nome
+var nome_utente = prompt("Nome")
+
+// Inserire il cognome
+var cognome_utente = prompt("Cognome")
+
+// Inserire il colore preferito
+var colore_preferito = prompt("Colore prefeito")
+
+// visualizzare la password generata
+var password_generator = (nome_utente + cognome_utente + colore_preferito + "20")
+document.writeln(password_generator)
