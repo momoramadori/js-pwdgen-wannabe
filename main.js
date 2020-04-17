683893847196129594ce8c1f5a0daa7e43742c3f
Momo Ramadori <momoramadori8@gmail.com>
2020-04-17 14:32:19 +0200

    prima bozza veloce

--- main.js
+++ main.js
@@ -9,4 +9,5 @@ var colore_preferito = prompt("Colore prefeito")
 
 // visualizzare la password generata
 var password_generator = (nome_utente + cognome_utente + colore_preferito + "20")
-document.writeln(password_generator)
+document.getElementById('password').innerHTML = password_generator
+document.getElementById('pwdgen').setAttribute("class", "visible")
